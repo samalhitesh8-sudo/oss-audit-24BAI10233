82df2344a58def10605c7a8d8760e6e73fd7dba7
samalhitesh8-sudo <samalhitesh8@gmail.com>
2026-03-29 21:50:00 +0530

    Create script1.sh

---
+++ script1.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Script 1: System Identity Report
+# Author: Hitesh Samal | Course: Open Source Software
+
+# --- Variables ---
+STUDENT_NAME="Hitesh Samal"
+SOFTWARE_CHOICE="Linux Kernel"
+
+# --- System info ---
+KERNEL=$(uname -r)
+USER_NAME=$(whoami)
+UPTIME=$(uptime -p)
+DISTRO=$(grep PRETTY_NAME /etc/os-release | cut -d= -f2 | tr -d '"')
+DATE_TIME=$(date)
+
+# --- Display ---
+echo "================================"
+echo "  Open Source Audit — $STUDENT_NAME"
+echo "================================"
+echo "Distro  : $DISTRO"
+echo "Kernel  : $KERNEL"
+echo "User    : $USER_NAME"
+echo "Home    : $HOME"
+echo "Uptime  : $UPTIME"
+echo "Date    : $DATE_TIME"
+echo "License : GNU General Public License (GPL)"
+echo "Software: $SOFTWARE_CHOICE"
+echo "================================"
